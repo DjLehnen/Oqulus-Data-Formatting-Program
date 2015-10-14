b42cfa67494506841cb3730185790e83fcc4d0c5
Fistbutter <djlehnen@gmail.com>
2015-10-13 20:15:50 -0400

    added angular app and controller, next I need to utilize ng-repeat to display dynamic data

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,20 @@
-var app = angular.module("ngOqulus");
+var app = angular.module("ngOqulus", []);
 
 var testData = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 var output = "";
-tableBody = document.getElementById("table-body");
 
 
-for (i=0; i<testData.length; i = i +4){
-    output = testData.slice(i,i+4)
-    console.log(output);
+app.controller("DataController", function($scope){
     
-    tableBody.appendChild();
-};
+    $scope.outputLoop = function(){
+        
+        for (i=0; i<testData.length; i = i +4){
+            output = testData.slice(i,i+4)
+            console.log(output);
+            $scope.data1 = output[1];
+            $scope.data2 = output[2];
+            $scope.data3 = output[3];
+            $scope.data4 = output[4];
+        };
+    }; 
+})
