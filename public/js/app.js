90ac4c626e27fc448f0700de03b881631b8065fe
Fistbutter <djlehnen@gmail.com>
2015-10-13 20:32:26 -0400

    data output functioning, creates table entries until all data has been displayed. Next I need to make # of table columns dynamically changeable while retaining functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,25 @@
 var app = angular.module("ngOqulus", []);
 
+var setLength = 4;
+
 var testData = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-var output = "";
 
 
 app.controller("DataController", function($scope){
-    
+    $scope.dataSets = [];
     $scope.outputLoop = function(){
         
-        for (i=0; i<testData.length; i = i +4){
-            output = testData.slice(i,i+4)
-            console.log(output);
-            $scope.data1 = output[1];
-            $scope.data2 = output[2];
-            $scope.data3 = output[3];
-            $scope.data4 = output[4];
+        for (i=0; i<testData.length; i = i + setLength){
+            outputSet = testData.slice(i,i + setLength)
+            console.log(outputSet);
+            var output = {
+                data1: outputSet[0],
+                data2: outputSet[1],
+                data3: outputSet[2],
+                data4: outputSet[3],
+            }
+            
+            $scope.dataSets.push(output);
         };
     }; 
 })
