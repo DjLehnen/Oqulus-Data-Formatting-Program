cc09afd9263bf6b3fd8ec40c70c62347b34a515f
Fistbutter <djlehnen@gmail.com>
2015-10-16 18:49:07 -0400

    added angular-upload module and linked to necessary files

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("ngOqulus", []);
+var app = angular.module("ngOqulus", ['lr.upload']);
 
 var setLength = 4;
 
@@ -7,8 +7,14 @@ var testData = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 app.controller("DataController", function($scope){
     $scope.dataSets = [];
-    $scope.outputLoop = function(){
+    $scope.columnCount = [];
+    
+    $scope.columnNumber = function(){
         
+    };
+    
+    $scope.outputLoop = function(){
+            
         for (i=0; i<testData.length; i = i + setLength){
             outputSet = testData.slice(i,i + setLength)
             console.log(outputSet);
@@ -17,9 +23,12 @@ app.controller("DataController", function($scope){
                 data2: outputSet[1],
                 data3: outputSet[2],
                 data4: outputSet[3],
+                data5: outputSet[4],
             }
             
             $scope.dataSets.push(output);
         };
     }; 
 })
+
+
